98166adec9b1818d229bed52567f012aa036fd77
Jingyu-An <jqrohan@gmail.com>
2022-11-27 21:24:02 -0500

    update study React

--- React/ProjectA/src/App.js
+++ React/ProjectA/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+  
+  const addUserHandler = (userName, userAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, {name: userName, age: userAge, id: Math.random().toString()}];
+    });
+  };
+  
   return (
-    <div>
-
-    </div>
+    <Fragment>
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users={usersList}/>
+    </Fragment>
   );
 }
 
